0f4c74abbae272cfdcc92e43a45675454f928254
ravuthz <ravuthz@gmail.com>
2016-10-19 14:45:45 +0700

    Vue.js project with vue-resource

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import VueResource from 'vue-resource'
+
+import App from './App'
+
+Vue.use(VueResource)
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App }
+})
